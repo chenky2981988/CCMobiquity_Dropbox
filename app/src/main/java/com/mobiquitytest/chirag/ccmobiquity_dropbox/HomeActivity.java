53b0dcdedd6013fb1cdb770907cc1901d188a044
chenky2981988 <chenky2981988@gmail.com>
2015-03-15 23:47:42 +0530

    Dropbox demo
    First Commit

---
+++ app/src/main/java/com/mobiquitytest/chirag/ccmobiquity_dropbox/HomeActivity.java
@@ -0,0 +1,53 @@
+package com.mobiquitytest.chirag.ccmobiquity_dropbox;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class HomeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id)
+        {
+            case R.id.action_unlink:
+                Log.d("TAG", "Unlink");
+                break;
+
+            case R.id.action_sync:
+                Log.d("TAG", "Sync");
+                break;
+
+            case R.id.action_upload:
+                Log.d("TAG", "Upload");
+                break;
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
